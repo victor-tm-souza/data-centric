616608679e8e273cafe6c6c763a25666b3c235f0
victor.souza <victor.souza@ua.pt>
2021-11-07 01:55:26 +0000

    first

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <title>PECI</title>
+
+    <link rel="stylesheet" type="text/css" href="fullPage_src/fullpage01.css" />
+    <link rel="stylesheet" type="text/css" href="css/styles.css" />
+
+</head>
+
+<body>
+
+    <div id="fullpage">
+        <div class="section " id="section0">
+            <h1><b>Data-Centric</b> Machine</br>
+                Learning ops for</br>
+                Autonomous Driving
+            </h1>
+        </div>
+        <div class="section" id="section1">
+            <div id="left1">
+                What is Machine Learning?
+            </div>
+            <div id="right1">
+                Machine Learning is the automatic learning process of machines.
+                </br>
+                To obtain a Machine Learning model it is mandatory for us to pocess 3 key resources:
+                </br></br></br>
+                <b>MODEL + DATASET + METRICS</b>
+            </div>
+        </div>
+        <div class="section" id="section2">
+            <div id="left2">
+                <p style="font-size:2em;"><b>Strategy</b></p></br></br>
+                For many years, researchers have been using a model-centric strategy
+                when approaching machine learning, this is, they have been focusing on building models
+                (algorithms).</br>The better the model, the better the result.</br></br>
+                However, now, this models already exist, they are accurate and they are open-source.
+                </br></br>
+                <p style="color: rgb(0, 155, 153);">So, How to improve those models' accuracy?</p>
+            </div>
+            <div id="right2">
+            </div>
+        </div>
+        <div class="section" id="section2">
+            <div id="left2">
+                <p style="font-size:2em;">
+                    <b>Data-Centric!</b>
+                </p></br></br>
+                With this strategy, we choose an already existing, good, model and then focus on enhancing the quality
+                of the dataset to be used for training the model.
+
+            </div>
+            <div id="right2">
+            </div>
+        </div>
+        <div class="section" id="section4">
+            <div id="left4">
+                <p style="font-size:2em;">
+                    <b>Tools</b>
+                </p></br>
+                <p>Sensor: <a href="https://en.wikipedia.org/wiki/Lidar">LiDAR</a></p>
+                <p>Data: <a href="https://en.wikipedia.org/wiki/Point_cloud">Point Clouds</a></p>
+                <p>Model: <a href="https://arxiv.org/abs/1812.05784">Point Pillars</a></p>
+                <p>Metrics: <a href="https://jonathan-hui.medium.com/map-mean-average-precision-for-object-detection-45c121a31173">
+                        IoU, Average Precision</a>
+                </p>
+            </div>
+            <div id="right4">
+                <img src="img/point_cloud.jpg" style="width: 100%;"></br>
+                <p>(Pointcloud taken from a LIDAR)</p>
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript" src="fullPage_src/fullpage.js"></script>
+
+    <script type="text/javascript">
+        var myFullpage = new fullpage('#fullpage', {
+            verticalCentered: false,
+            autoScrolling: true,
+            scrollHorizontally: true,
+            navigation: true,
+            navigationTooltips: ['Home', 'Intro', 'Strategy', 'Data-Centric', 'Tools'],
+            showActiveTooltip: true,
+            scrollingSpeed: 1000,
+            css3: false
+        });
+    </script>
+</body>
+
+</html>
---
+++ index_old.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-eqiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Data-Centric</title>
+    <link href="css/styles_old.css" rel="stylesheet" type="text/css">
+    <link href="fullPage_src/fullpage.css" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container" id="fullpage">
+        <div class="separator section header">
+            <p class="header_text"><b>Data-Centric</b> Machine</br>
+                Learning ops for</br>
+                Autonomous Driving
+            </p>
+        </div>
+        <div class="separator section intro">
+            <div class="sec_title">
+                <h1>O que é Machine Learning?</h1>
+            </div>
+            <div class="sec_content">
+                <p>Machine Learning é o processo de aprendizagem automática das máquinas</p>
+            </div>
+        </div>
+        <div class="separator section" id="intro">
+        </div>
+        <div class="separator section" id="intro">
+        </div>
+    </div>
+
+    <script type="text/javascript" src="fullPage_src/fullpage.js"></script>
+    <script>
+        new fullpage('#fullpage', {
+            //options here
+            autoScrolling: true,
+            scrollHorizontally: true,
+            navigation: true,
+            navigationTooltips: ['Home', 'Intro'],
+            showActiveTooltip: true,
+            css3: false
+        });
+    </script>
+</body>
+
+</html>
